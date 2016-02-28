4a934e86c59d2ae443535e8293cc1ceb2ae2b0c5
adam <edek-150@o2.pl>
2016-02-28 11:26:06 +0100

    login

--- gallery/urls.py
+++ gallery/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('django_gallery.urls')),
+    url(r'', include('registration.backends.simple.urls')),
+    url(r'', include('django_gallery.urls')),
 ]
